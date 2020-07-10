c0635661ef482495530c9527669b78e24915aa78
iruseg00 <49835619+iruseg00@users.noreply.github.com>
2020-07-10 05:24:47 +0300

    Update coder.js

--- coder.js
+++ coder.js
@@ -1,3 +1,41 @@
+const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+
+const atob = function(input = '') {
+  let str = input.replace(/=+$/, '');
+  let output = '';
+
+  if (str.length % 4 == 1) {
+    throw new Error("'atob' failed: The string to be decoded is not correctly encoded.");
+  }
+  for (let bc = 0, bs = 0, buffer, i = 0; buffer = str.charAt(i++);
+
+    ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer,
+      bc++ % 4) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0
+  ) {
+    buffer = chars.indexOf(buffer);
+  }
+
+  return output;
+};
+
+const btoa = function(input = '') {
+  let str = input;
+  let output = '';
+
+  for (let block = 0, charCode, i = 0, map = chars; str.charAt(i | 0) || (map = '=', i % 1); output += map.charAt(63 & block >> 8 - i % 1 * 8)) {
+
+    charCode = str.charCodeAt(i += 3 / 4);
+
+    if (charCode > 0xFF) {
+      throw new Error("'btoa' failed: The string to be encoded contains characters outside of the Latin1 range.");
+    }
+
+    block = block << 8 | charCode;
+  }
+
+  return output;
+};
+
 // btoa replacement. Stock btoa fails on on non-Latin1 strings.
 function b64EncodeUnicode(str) 
 {
@@ -17,3 +55,13 @@ function b64DecodeUnicode(str)
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
 } 
+
+// let secret ="$2a$10$6a45qF29N/XE4Bv9NBjqu.Mn1.SAzN8MYbHE0f2/lDSPgYIUJoCQa" //qwe123
+
+var T = "info"
+
+var text = b64EncodeUnicode(T)
+console.log("encode: " + text)
+
+var dec = b64DecodeUnicode(text)
+console.log("decode: " + dec)
